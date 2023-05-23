e17d6afe94700b5f030770421b3af45b651aaab8
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-05-23 21:19:27 +0800

    swapi

--- notes.js
+++ notes.js
@@ -2204,4 +2204,40 @@
 
 // handleGuessingGame();
 
-// POSTMAN = Postman can be used to write functional tests, integration test, regression tests, and more. Postman's Node.js-based runtime contains supprot for common patterns and libraries that you can use to build tests quickly.
+// POSTMAN = Postman can be used to write functional tests, integration test, regression tests, and more. Postman's Node.js-based runtime contains support for common patterns and libraries that you can use to build tests quickly.
+
+const skyWalk = `
+{
+  "name": "Luke Skywalker",
+  "height": "172",
+  "mass": "77",
+  "hair_color": "blond",
+  "skin_color": "fair",
+  "eye_color": "blue",
+  "birth_year": "19BBY",
+  "gender": "male",
+  "homeworld": "https://swapi.dev/api/planets/1/",
+  "films": [
+      "https://swapi.dev/api/films/1/",
+      "https://swapi.dev/api/films/2/",
+      "https://swapi.dev/api/films/3/",
+      "https://swapi.dev/api/films/6/"
+  ],
+  "species": [],
+  "vehicles": [
+      "https://swapi.dev/api/vehicles/14/",
+      "https://swapi.dev/api/vehicles/30/"
+  ],
+  "starships": [
+      "https://swapi.dev/api/starships/12/",
+      "https://swapi.dev/api/starships/22/"
+  ],
+  "created": "2014-12-09T13:50:51.644000Z",
+  "edited": "2014-12-20T21:17:56.891000Z",
+  "url": "https://swapi.dev/api/people/1/"
+}
+`;
+
+const test = JSON.parse(skyWalk);
+
+console.log(test);
